cbc969dc01670f41c242a1d51ff3d1fa3d90bca3
noma0006 <91168507+Noma006@users.noreply.github.com>
2023-03-19 20:04:41 -0400

    Add files via upload

---
+++ mtm6201/script.js
@@ -0,0 +1,9 @@
+// show pass and hide pass
+function showPass() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
