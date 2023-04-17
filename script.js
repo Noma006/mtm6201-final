7742b648f61be75adad3b2196e09740963634f8c
noma0006 <91168507+Noma006@users.noreply.github.com>
2023-04-16 22:25:19 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// show pass and hide pass
-function showPass() {
-  var x = document.getElementById("password");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
